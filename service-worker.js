668777f6bb7fab4f0847918e0416c43c12ac49ed
deepakkalra483 <deepakkalra483@gmail.com>
2025-01-28 00:06:09 +0530

    Updates

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("push", (event) => {
-  const options = {
-    body: event.data.text(),
-    icon: "./assets/images/icons/order_icon.png", // Replace with your icon path
-  };
-  event.waitUntil(self.registration.showNotification("Your App Name", options));
-});
